d1b5cafa1e74a2599a59f0071864834a2dd00628
Ahmed Hafez <ahmedmhiz2010@gmail.com>
2021-05-03 15:35:55 +0300

    add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DataWrapper from "./components/DataWrapper.comp";
+
+interface Props {}
+
+const App = (props: Props) => {
+  return (
+    <>
+      <DataWrapper />
+    </>
+  );
+};
+
+export default App;
