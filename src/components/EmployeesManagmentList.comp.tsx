930e24ae8f5dcb204a2b3bf969edf1e985786186
ionR19 <ahmedmhiz2010@gmail.com>
2021-05-18 23:24:43 +0300

    add list to view employee

--- src/components/EmployeesManagmentList.comp.tsx
+++ src/components/EmployeesManagmentList.comp.tsx
@@ -1,35 +1,40 @@
 import React from "react";
-import { Button, Columns, Form } from "react-bulma-components";
+import { Button, Table } from "react-bulma-components";
+import { useDispatch } from "react-redux";
+import { removeEmployee, selectEmployees, useTypedSelector } from "../store";
 
 interface Props {}
 
 const EmployeesManagmentList: React.FC<Props> = (props) => {
+  const emps = useTypedSelector(selectEmployees);
+  const dispatch = useDispatch();
+
   return (
-    <form>
-      <Columns>
-        <Columns.Column>
-          <Form.Label>
-            Name
-            <Form.Input></Form.Input>
-          </Form.Label>
-        </Columns.Column>
-        <Columns.Column>
-          <Form.Label>
-            Civil ID
-            <Form.Input></Form.Input>
-          </Form.Label>
-        </Columns.Column>
-        <Columns.Column>
-          <Form.Label>
-            File No
-            <Form.Input></Form.Input>
-          </Form.Label>
-        </Columns.Column>
-        <Columns.Column>
-          <Button type="submit">Add</Button>
-        </Columns.Column>
-      </Columns>
-    </form>
+    <div style={{ height: 350, overflowY: "scroll" }}>
+      <Table size="fullwidth" striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Civil ID</th>
+            <th>File No</th>
+            <th>Tools</th>
+          </tr>
+        </thead>
+        <tbody>
+          {emps.map((emp) => (
+            <tr key={emp.id!}>
+              <td>{emp.name}</td>
+              <td>{emp.civil_id}</td>
+              <td>{emp.file_no}</td>
+              <td>
+                <Button>Toggle</Button>
+                <Button remove onClick={() => dispatch(removeEmployee(emp.id!))}></Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
