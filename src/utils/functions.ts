b63fdcf76145c9f53a0ac4af20ea759b18838fec
ionR19 <ahmedmhiz2010@gmail.com>
2021-05-18 23:23:06 +0300

    generate function more customizable

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,53 +7,55 @@ export function isLess(date1: DateTime, date2: DateTime) {
 
 type options = {
   skipdates?: string[];
+  skipFn?: (dt: DateTime) => boolean;
   timeFrom?: string;
   timeTo?: string;
   location?: string;
+  limit?: number;
 };
 
 export function generate(
   from: string,
   to: string,
   employees: iUser[],
-  { timeFrom = "09:00", timeTo = "13:00", location = "مبنى الوزارة الرئيسي", ...opt }: options
+  {
+    timeFrom = "09:00",
+    timeTo = "13:00",
+    location = "مبنى الوزارة الرئيسي",
+    limit = 100000,
+    ...opt
+  }: options
 ) {
-  let startDate = DateTime.fromFormat("yyyy-mm-dd", from);
-  let endDate = DateTime.fromFormat("yyyy-mm-dd", to);
+  let currentDate = DateTime.fromFormat(from, "yyyy-mm-dd");
+  let endDate = DateTime.fromFormat(to, "yyyy-mm-dd");
   let table: iRecord[] = [];
 
-  if (startDate && endDate) {
-    while (isLess(startDate, endDate)) {
-      for (let index = 0; index < employees.length; index++) {
-        const element = employees[index];
-        if (
-          startDate.weekday === 5 ||
-          startDate.weekday === 6 ||
-          opt?.skipdates?.some((i) => i === startDate.toISODate())
-        )
-          continue;
-        table.push({
-          civil_id: element.civil_id,
-          file_no: element.file_no,
-          type: "F1",
-          date: startDate.toISODate().toString(),
-          timeIn: timeFrom,
-          location,
-        });
-        table.push({
-          civil_id: element.civil_id,
-          file_no: element.file_no,
-          type: "F2",
-          date: startDate.toISODate().toString(),
-          timeIn: timeTo,
-          location,
-        });
-      }
-
-      startDate = startDate.plus({
-        days: 1,
+  while (isLess(currentDate, endDate) && limit > -1) {
+    --limit;
+    for (let index = 0; index < employees.length; index++) {
+      const element = employees[index];
+      if (opt?.skipdates?.some((i) => i === currentDate.toISODate()) || opt?.skipFn?.(currentDate))
+        continue;
+      table.push({
+        civil_id: element.civil_id,
+        file_no: element.file_no,
+        type: "F1",
+        date: currentDate.toISODate().toString(),
+        timeIn: timeFrom,
+        location,
+      });
+      table.push({
+        civil_id: element.civil_id,
+        file_no: element.file_no,
+        type: "F2",
+        date: currentDate.toISODate().toString(),
+        timeIn: timeTo,
+        location,
       });
     }
-    return table;
+    currentDate = currentDate.plus({
+      days: 1,
+    });
   }
+  return table;
 }
